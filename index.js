abfa3537f099b42fc899e46dc17190a93ddb2dfe
vp932712 Patel <vp932712@gmail.com>
2017-11-10 00:27:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function shout(string){
   return string.toUpperCase();
 }
+function whisper(string){
+  return string.toLowerCase();
+}
+function longshout(string){
+  console.log(string.toUpperCase());
+}
+function longWhisper(string){
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
+    return ("I cant't hear you!");
+  }
+}
