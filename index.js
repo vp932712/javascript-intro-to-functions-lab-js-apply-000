9043f44b08183391f5f736afd3f37798e1be705b
vp932712 Patel <vp932712@gmail.com>
2017-11-10 00:32:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return ("I can't hear you!");
+  } else if (string === string.toUpperCase()){
+    return ("YES INDEED!")
+  } else {
+    return ("I love you, too.")
   }
 }
